49be4228fb7c951c5ffd5fd0b53486eed3070a6a
King_Of_Klubs <caleb7557@gmail.com>
2019-08-30 18:54:27 -0400

    first push

---
+++ Dodge Saw/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    Rigidbody2D _rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Xspeed = Input.GetAxis("Horizontal");
+        float Yspeed = Input.GetAxis("Vertical");
+
+        _rigid.velocity = new Vector2(Xspeed, Yspeed);
+    }
+}

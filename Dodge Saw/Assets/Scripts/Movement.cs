cccdce4103297e9d8a85af89ff01382134bdeafd
Caleb Mauldin <caleb.m47@aol.com>
2019-09-02 22:48:29 -0400

    was able to to push to ios and added a icon for the game added slowing movement based on touch input aswell

--- Dodge Saw/Assets/Scripts/Movement.cs
+++ Dodge Saw/Assets/Scripts/Movement.cs
@@ -1,32 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.iOS;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
+    #region Public Variables
 
+    //public variables
+    public float _dst;
+    public float _moveSpeed;
+    #endregion
+
+    #region Private Variables
+    Vector2 _velocity;
     Rigidbody2D _rigid;
-    Vector2 velocity;
-    public float dst;
-    Vector2 direction;
+    Vector2 _direction;
+    Ray _ray;
+    float _initialSpeed;
+
+    #endregion
+
+    Touch touch;
+
 
     // Start is called before the first frame update
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
-        velocity = new Vector2(1,1);
-        direction = this.transform.up;
+        _velocity = new Vector2(1,1);
+        _direction = this.transform.up;
+        _initialSpeed = _moveSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        //float Xspeed = Input.GetAxis("Horizontal");
-        //float Yspeed = Input.GetAxis("Vertical");
+        _rigid.velocity = _velocity * _moveSpeed;
+        KeepInBounds();
+        Reflect();
+        Move();
 
-        //_rigid.velocity = new Vector2(Xspeed, Yspeed);
-            _rigid.velocity = velocity;
+      float Xspeed = Input.GetAxis("Horizontal");
+      float Yspeed = Input.GetAxis("Vertical");
+
+        //_rigid.velocity = new Vector2(Xspeed, Yspeed);      
+    }
+
+    public void KeepInBounds()
+    {
         Vector2 screenPosition = Camera.main.WorldToScreenPoint(this.transform.position);
         if ((screenPosition.y > Screen.height) || (screenPosition.y < 0f) || (screenPosition.x > Screen.width) || (screenPosition.x < 0f))
         {
@@ -35,21 +58,40 @@ public class Movement : MonoBehaviour
             Vector3 newWorldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
             this.transform.position = new Vector2(newWorldPosition.x, newWorldPosition.y);
         }
-            Reflect();
-        Debug.Log(velocity);
-    }
-    private void Reflect()
-    {
-        Ray ray = new Ray(transform.position, direction);
-        RaycastHit hit;
-            if(Physics.Raycast(ray, out hit, dst))
-            {
-                velocity = Vector2.Reflect(direction, hit.normal);
-                direction = velocity;
-            }
-            
     }
 
- 
+    private void Reflect()
+    {
+        _ray = new Ray(transform.position, _direction);
+        RaycastHit _hit;
+            if(Physics.Raycast(_ray, out _hit, _dst))
+            {
+                _velocity = Vector2.Reflect(_direction, _hit.normal);
+                _direction = _velocity;
+            }
+        
+    }
+
+    //draws the raycast of the direction of velocity
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(_ray);
+    }
+
+    //moves the pentagon based on touch input
+    public void Move()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                _moveSpeed = 1;
+            }
+        }
+        else
+            _moveSpeed = _initialSpeed;
+    }
+
 
 }

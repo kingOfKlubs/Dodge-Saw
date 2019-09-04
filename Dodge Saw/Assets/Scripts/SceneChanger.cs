9c0da12d2381415e3cfdcd2fc2d7fbfc2842a23c
Caleb Mauldin <caleb.m47@aol.com>
2019-09-03 23:41:32 -0400

    added several assets and scenes

---
+++ Dodge Saw/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class SceneChanger : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameLevel");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void SetVolume(float volume)
+    {
+        mixer.SetFloat("volume", volume);
+        Debug.Log(volume);
+    }
+}

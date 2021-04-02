0908a6dd051687cedcd54b572ffc1ca7c647c79e
Caleb Mauldin <caleb.m47@aol.com>
2021-04-02 19:07:52 -0400

    crossing my T's and dotting my I's with saveing
    Hopefully the extra steps I added to the saves become a little more secure will need to implement backups or some kind of way to save the data off the host device.

--- Dodge Saw/Assets/_Scripts/DataManager.cs
+++ Dodge Saw/Assets/_Scripts/DataManager.cs
@@ -16,9 +16,6 @@ public class DataManager : MonoBehaviour {
     [SerializeField] private List<ItemData> objectsToPersist;
     [SerializeField] private List<ShopItem> objectsToReference;
 
-    // Start is called before the first frame update
-   
-
     public bool SavedFile()
     {
         return Directory.Exists(Application.persistentDataPath + "/Store_Data");
@@ -49,40 +46,72 @@ public class DataManager : MonoBehaviour {
         for (int i = 0; i < objectsToPersist.Count; i++)
         {
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Create(Application.persistentDataPath + "/Store_Data" + string.Format("/{0}_{1}.pso", persisterName, i));
-            bf.Serialize(file, objectsToPersist[i]);
-            file.Close();
+            using (FileStream file = File.Create(Application.persistentDataPath + "/Store_Data" + string.Format("/{0}_{1}.pso", persisterName, i))) {
+                try
+                {
+                    bf.Serialize(file, objectsToPersist[i]);
+                }
+                catch (IOException e)
+                {
+                    Debug.LogError("You had an error saving the game data. The Error was: " + e);
+                }
+                finally
+                {
+                    if(file != null)
+                    {
+                        file.Close();
+                    }
+                }
+            }
         }
     }
 
     public void OnEnable()
     {
-
         //TODO we need to find a way to make objectsToPersist a private list and then fill it with add instead of having to ensure both of the lists are equal in size
         for (int i = 0; i < objectsToReference.Count; i++)
         {
             if (File.Exists(Application.persistentDataPath + "/Store_Data" + string.Format("/{0}_{1}.pso", persisterName, i)))
             {
                 BinaryFormatter bf = new BinaryFormatter();
-                FileStream file = File.Open(Application.persistentDataPath + "/Store_Data" + string.Format("/{0}_{1}.pso", persisterName, i), FileMode.Open);
-                ItemData tempItem = (bf.Deserialize(file)) as ItemData;
-                items[i].backgroundImage = objectsToReference[i].backgroundImage;
-                items[i].texture = objectsToReference[i].texture;
-                items[i].color = objectsToReference[i].color;
-                items[i].color2 = objectsToReference[i].color2;
-                items[i].gradient = objectsToReference[i].gradient;
-                items[i].itemName = objectsToReference[i]._itemName;
-                items[i].buttonText = tempItem.buttonText;
-                items[i].cost = objectsToReference[i].cost;
-                items[i].equipped = tempItem.equipped;
-                items[i].purchased = tempItem.purchased;
-                items[i].itemType = tempItem.itemType;
-
-                file.Close();
+                using (FileStream file = File.Open(Application.persistentDataPath + "/Store_Data" + string.Format("/{0}_{1}.pso", persisterName, i), FileMode.Open))
+                {
+                    try
+                    {
+                        ItemData tempItem = (bf.Deserialize(file)) as ItemData;
+                        items[i].backgroundImage = objectsToReference[i].backgroundImage;
+                        items[i].texture = objectsToReference[i].texture;
+                        items[i].color = objectsToReference[i].color;
+                        items[i].color2 = objectsToReference[i].color2;
+                        items[i].gradient = objectsToReference[i].gradient;
+                        items[i].itemName = objectsToReference[i]._itemName;
+                        items[i].buttonText = tempItem.buttonText;
+                        items[i].cost = objectsToReference[i].cost;
+                        items[i].equipped = tempItem.equipped;
+                        items[i].purchased = tempItem.purchased;
+                        items[i].itemType = tempItem.itemType;
+                    }
+                    catch (IOException e)
+                    {
+                        Debug.LogError("You had an error loading your saved data. The error was: " + e);
+                    }
+                    finally
+                    {
+                        if(file != null)
+                        {
+                            file.Close();
+                        }
+                    }
+                }
+                objectsToPersist[i].itemName = items[i].itemName;
+                objectsToPersist[i].buttonText = items[i].buttonText;
+                objectsToPersist[i].cost = items[i].cost;
+                objectsToPersist[i].equipped = items[i].equipped;
+                objectsToPersist[i].purchased = items[i].purchased;
+                objectsToPersist[i].itemType = items[i].itemType;
             }
             else
             {
-                //these lists need to be identical in size
                 items[i].backgroundImage = objectsToReference[i].backgroundImage;
                 items[i].texture = objectsToReference[i].texture;
                 items[i].color = objectsToReference[i].color;
@@ -94,18 +123,14 @@ public class DataManager : MonoBehaviour {
                 items[i].equipped = objectsToReference[i].equipped;
                 items[i].purchased = objectsToReference[i].purchased;
                 items[i].itemType = objectsToReference[i].itemType;
+                objectsToPersist[i].itemName = objectsToReference[i]._itemName;
+                objectsToPersist[i].buttonText = objectsToReference[i]._buttonText;
+                objectsToPersist[i].cost = objectsToReference[i].cost;
+                objectsToPersist[i].equipped = objectsToReference[i].equipped;
+                objectsToPersist[i].purchased = objectsToReference[i].purchased;
+                objectsToPersist[i].itemType = objectsToReference[i].itemType;
             }
-
-            objectsToPersist[i].itemName = objectsToReference[i]._itemName;
-            objectsToPersist[i].buttonText = objectsToReference[i]._buttonText;
-            objectsToPersist[i].cost = objectsToReference[i].cost;
-            objectsToPersist[i].equipped = objectsToReference[i].equipped;
-            objectsToPersist[i].purchased = objectsToReference[i].purchased;
-            objectsToPersist[i].itemType = objectsToReference[i].itemType;
-
         }
-
-
 
         for (int i = 0; i < items.Count; i++) {
             if (items[i].itemType == ItemType.Player) {
@@ -138,6 +163,6 @@ public class DataManager : MonoBehaviour {
     }
 
     private void OnDistroy() {
-
+        SaveGame();
     }
 }
